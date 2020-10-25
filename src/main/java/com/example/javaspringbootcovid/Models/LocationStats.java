942860b375fe291a87c2cc848540908a41a63f2d
markza1998 <2411kjn@gmail.com>
2020-10-25 10:30:54 +0700

    add model

---
+++ src/main/java/com/example/javaspringbootcovid/Models/LocationStats.java
@@ -0,0 +1,11 @@
+package com.example.javaspringbootcovid.Models;
+
+import lombok.Data;
+
+@Data
+public class LocationStats {
+    private String state;
+    private String country;
+    private int latestTotalcase;
+
+}
